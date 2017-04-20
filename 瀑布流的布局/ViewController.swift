0acfd7c388944338fc9e3ff9322cb1942165e324
CoderSLZeng <359297567@qq.com>
2017-04-20 20:20:52 +0800

    实现瀑布流功能

--- 瀑布流的布局/ViewController.swift
+++ 瀑布流的布局/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+private let kContentCellID = "kContentCellID"
 
+class ViewController: UIViewController {
+    
+    fileprivate lazy var collectionView : UICollectionView = {
+        let layout = SLWaterfallLayout()
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        layout.minimumInteritemSpacing = 10
+        layout.minimumLineSpacing = 10
+        
+        layout.dataSource = self
+        
+        let collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //        NSLayoutConstraint()
+        
+        view.addSubview(collectionView)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: kContentCellID)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
+
+
+extension ViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kContentCellID, for: indexPath)
+        
+        cell.backgroundColor = UIColor.randomColor()
+        
+        return cell
+    }
+}
+
+
+extension ViewController : SLWaterfallLayoutDataSource {
+    func numberOfCols(_ waterfall: SLWaterfallLayout) -> Int {
+        return 4
+    }
+    
+    func waterfall(_ waterfall: SLWaterfallLayout, item: Int) -> CGFloat {
+        return CGFloat(arc4random_uniform(150) + 100)
+    }
+}
+
 
